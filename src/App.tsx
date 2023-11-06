c0a7f664b8d6901274c7a263548fb414c38de66f
Hudson Ansley <hudsonansley@gmail.com>
2023-11-06 15:21:21 -0500

    add max-width to content

--- src/App.tsx
+++ src/App.tsx
@@ -543,81 +543,83 @@ const App = ({ initWordSetType }: { initWordSetType: WordleDict.wordSet }) => {
   }, [statsInfo.combinedBoardIndexStrings, boardStr, curLetterLoc]);
 
   return (
-    <div className="App">
-      <nav>
-        <div className="nav-buttons">
-          <button id="help" onClick={onShowHelp} tabIndex={-1}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
-            >
-              <path
-                fill="#ffffff"
-                d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"
-              ></path>
-            </svg>
-          </button>
-          <button id="reset" onClick={onReset} tabIndex={-1}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
-            >
-              <path
-                fill="#ffffff"
-                d="M20.817 11.186a8.94 8.94 0 0 0-1.355-3.219 9.053 9.053 0 0 0-2.43-2.43 8.95 8.95 0 0 0-3.219-1.355 9.028 9.028 0 0 0-1.838-.18V2L8 5l3.975 3V6.002c.484-.002.968.044 1.435.14a6.961 6.961 0 0 1 2.502 1.053 7.005 7.005 0 0 1 1.892 1.892A6.967 6.967 0 0 1 19 13a7.032 7.032 0 0 1-.55 2.725 7.11 7.11 0 0 1-.644 1.188 7.2 7.2 0 0 1-.858 1.039 7.028 7.028 0 0 1-3.536 1.907 7.13 7.13 0 0 1-2.822 0 6.961 6.961 0 0 1-2.503-1.054 7.002 7.002 0 0 1-1.89-1.89A6.996 6.996 0 0 1 5 13H3a9.02 9.02 0 0 0 1.539 5.034 9.096 9.096 0 0 0 2.428 2.428A8.95 8.95 0 0 0 12 22a9.09 9.09 0 0 0 1.814-.183 9.014 9.014 0 0 0 3.218-1.355 8.886 8.886 0 0 0 1.331-1.099 9.228 9.228 0 0 0 1.1-1.332A8.952 8.952 0 0 0 21 13a9.09 9.09 0 0 0-.183-1.814z"
-              />
-            </svg>
-          </button>
-          <div className="word-set-button-container">
-            <div className="one-by-two-buttons">
-              {wordSetButton("wordle")}
-              {wordSetButton("quordle")}
+    <body>
+      <div className="App">
+        <nav>
+          <div className="nav-buttons">
+            <button id="help" onClick={onShowHelp} tabIndex={-1}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <path
+                  fill="#ffffff"
+                  d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"
+                ></path>
+              </svg>
+            </button>
+            <button id="reset" onClick={onReset} tabIndex={-1}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+              >
+                <path
+                  fill="#ffffff"
+                  d="M20.817 11.186a8.94 8.94 0 0 0-1.355-3.219 9.053 9.053 0 0 0-2.43-2.43 8.95 8.95 0 0 0-3.219-1.355 9.028 9.028 0 0 0-1.838-.18V2L8 5l3.975 3V6.002c.484-.002.968.044 1.435.14a6.961 6.961 0 0 1 2.502 1.053 7.005 7.005 0 0 1 1.892 1.892A6.967 6.967 0 0 1 19 13a7.032 7.032 0 0 1-.55 2.725 7.11 7.11 0 0 1-.644 1.188 7.2 7.2 0 0 1-.858 1.039 7.028 7.028 0 0 1-3.536 1.907 7.13 7.13 0 0 1-2.822 0 6.961 6.961 0 0 1-2.503-1.054 7.002 7.002 0 0 1-1.89-1.89A6.996 6.996 0 0 1 5 13H3a9.02 9.02 0 0 0 1.539 5.034 9.096 9.096 0 0 0 2.428 2.428A8.95 8.95 0 0 0 12 22a9.09 9.09 0 0 0 1.814-.183 9.014 9.014 0 0 0 3.218-1.355 8.886 8.886 0 0 0 1.331-1.099 9.228 9.228 0 0 0 1.1-1.332A8.952 8.952 0 0 0 21 13a9.09 9.09 0 0 0-.183-1.814z"
+                />
+              </svg>
+            </button>
+            <div className="word-set-button-container">
+              <div className="one-by-two-buttons">
+                {wordSetButton("wordle")}
+                {wordSetButton("quordle")}
+              </div>
             </div>
           </div>
-        </div>
-        <h1>
-          {`${wordSetType[0].toUpperCase()}${wordSetType.slice(1)}`} Helper
-        </h1>
-        <div
-          className={`memory-button-container${
-            wordSetType === "quordle" ? "" : " hidden"
-          }`}
-        >
-          <div className="quordle-button-container">{memoryButton(-1)}</div>
-          <div className="two-by-two-buttons">
-            {memoryButton(0)}
-            {memoryButton(1)}
-            {memoryButton(2)}
-            {memoryButton(3)}
-          </div>
-        </div>
-      </nav>
-      <AppContext.Provider value={getProviderProps}>
-        <div className="content">
-          <div className="row">
-            <div className="game column">
-              {initProgress < 1 ? (
-                <InitProgress progress={initProgress} />
-              ) : (
-                <Board />
-              )}
-              <Keyboard hidden={false} />
-            </div>
-            <div className="stats column">
-              {infoType === "stats" ? (
-                <WordStats statsInfo={statsInfo} />
-              ) : (
-                <Information infoType={infoType} />
-              )}
+          <h1>
+            {`${wordSetType[0].toUpperCase()}${wordSetType.slice(1)}`} Helper
+          </h1>
+          <div
+            className={`memory-button-container${
+              wordSetType === "quordle" ? "" : " hidden"
+            }`}
+          >
+            <div className="quordle-button-container">{memoryButton(-1)}</div>
+            <div className="two-by-two-buttons">
+              {memoryButton(0)}
+              {memoryButton(1)}
+              {memoryButton(2)}
+              {memoryButton(3)}
             </div>
           </div>
-        </div>
-      </AppContext.Provider>
-    </div>
+        </nav>
+        <AppContext.Provider value={getProviderProps}>
+          <div className="content">
+            <div className="row">
+              <div className="game column">
+                {initProgress < 1 ? (
+                  <InitProgress progress={initProgress} />
+                ) : (
+                  <Board />
+                )}
+                <Keyboard hidden={false} />
+              </div>
+              <div className="stats column">
+                {infoType === "stats" ? (
+                  <WordStats statsInfo={statsInfo} />
+                ) : (
+                  <Information infoType={infoType} />
+                )}
+              </div>
+            </div>
+          </div>
+        </AppContext.Provider>
+      </div>
+    </body>
   );
 };
 
